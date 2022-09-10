03ad9674321b8f0cd546da6725979d5019b35d4a
Anton Brylyakov <yoll@rambler.ru>
2022-09-10 03:10:06 -0700

    Clearing all resources

--- CPPM5_Homework1.cpp
+++ CPPM5_Homework1.cpp
@@ -27,6 +27,14 @@ int* readArray(std::ifstream& fin, int& itemCount)
 	return arr;
 }
 
+void safeFreeArray(int* arr)
+{
+	if (arr)
+	{
+		delete[] arr;
+	}
+}
+
 void arraySpecialPrint(std::ofstream& fout, int* arr, int size, int startIndex)
 {
 	fout << size << std::endl;
@@ -53,41 +61,51 @@ int main()
 		return 1;
 	}
 
-	int size1, size2;
-	int* arr1 = readArray(fin, size1);
+	int size1 = 0, size2 = 0;
+	int* arr1 = nullptr;
+	int* arr2 = nullptr;
+
+	arr1 = readArray(fin, size1);
+	if (arr1)
+	{
+		arr2 = readArray(fin, size2);
+	}
+
 	if (!arr1)
 	{
 		std::cout << "Не удалось считать данные первого массива из файла '" + inputFileName + "'" << std::endl;
-		fin.close();
-		return 2;
 	}
-
-	int* arr2 = readArray(fin, size2);
+	else
 	if (!arr2)
 	{
 		std::cout << "Не удалось считать данные второго массива из файла '" + inputFileName + "'" << std::endl;
-		fin.close();
-		return 2;
 	}
 
 	fin.close();
 
-	std::cout << "Прочитаны данные из файла '" + inputFileName + "'" << std::endl;
-
-	std::ofstream fout(outputFileName);
-
-	if (!fout.is_open())
+	if (arr1 && arr2)
 	{
-		std::cout << "Не удалось открыть для записи файл '" + outputFileName + "'" << std::endl;
-		return 3;
+		std::cout << "Прочитаны данные из файла '" + inputFileName + "'" << std::endl;
+
+		std::ofstream fout(outputFileName);
+
+		if (fout.is_open())
+		{
+			arraySpecialPrint(fout, arr2, size2, size2 - 1);
+			arraySpecialPrint(fout, arr1, size1, 1);
+
+			std::cout << "Записан файл '" + inputFileName + "'" << std::endl;
+
+			fout.close();
+		}
+		else
+		{
+			std::cout << "Не удалось открыть для записи файл '" + outputFileName + "'" << std::endl;
+		}
 	}
 
-	arraySpecialPrint(fout, arr2, size2, size2 - 1);
-	arraySpecialPrint(fout, arr1, size1, 1);
-
-	std::cout << "Записан файл '" + inputFileName + "'" << std::endl;
-
-	fout.close();
-
+	safeFreeArray(arr1);
+	safeFreeArray(arr2);
+	
 	return 0;
 }
